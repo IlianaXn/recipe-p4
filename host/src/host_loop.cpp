1120d312f1497d9c2a15f77384d252404aa7bbbd
Ghoood <weili_wu@outlook.com>
2025-11-26 15:04:57 -0500

    port

--- host/src/host_loop.cpp
+++ host/src/host_loop.cpp
@@ -20,7 +20,7 @@
 #include <vector>
 
 // Experiment parameters
-constexpr int NUM_PACKETS = 200;
+constexpr int NUM_PACKETS = 1;
 constexpr int MAX_ITER    = 15;
 
 static void ensure_output_directory() {
