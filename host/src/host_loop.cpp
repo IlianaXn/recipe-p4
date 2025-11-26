59d56939fa6c4370e26ee2c6d6a31de2cb0f937c
Ghoood <weili_wu@outlook.com>
2025-11-26 13:42:41 -0500

    port

--- host/src/host_loop.cpp
+++ host/src/host_loop.cpp
@@ -46,7 +46,7 @@ int main() {
     ensure_output_directory();
 
     // CHANGE this to the NIC connected to your Tofino
-    std::string ifname = "ens1f0";
+    std::string ifname = "veth1";
 
     uint8_t host_mac[6]   = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
     uint8_t tofino_mac[6] = {0x00, 0xaa, 0xbb, 0xcc, 0xdd, 0xee};
